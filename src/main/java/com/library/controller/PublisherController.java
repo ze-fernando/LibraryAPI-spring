32b3f4231d7bc7058b5b4cb51e92d073b329852f
ze-fernando <jfenginner935@gmail.com>
2023-09-25 21:51:32 -0300

    create publisherController***

--- src/main/java/com/library/controller/PublisherController.java
+++ src/main/java/com/library/controller/PublisherController.java
@@ -1,4 +1,4 @@
-package com.library.domain.controller;
+package com.library.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
